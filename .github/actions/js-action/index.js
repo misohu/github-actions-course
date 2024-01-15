fe63a0e9c2174c4d4962f10c03b8c0ba70ee9e1b
misohu <michal.hucko@canonical.com>
2024-01-15 09:05:36 +0100

    JS action

---
+++ .github/actions/js-action/index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const name = core.getInput('name');  // name is from action.yaml (the input)
+  console.log(`Arg received ${name}`);
+  
+  console.log('Workdir:');
+  fs.readdirSync('.').forEach(file => {
+    console.log(file);
+  });
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2); // This is how we can get the github context in the action
+  console.log(`The event payload: ${payload}`);
+  
+  fs.writeFileSync('name.txt', name);
+  core.setOutput('processed-name', name);
+} catch (error) {
+  core.setFailed(error.message);
+}
